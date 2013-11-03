bac627508eb70c21192b1bd05c500d8f68df8899
Nick Nisi <nick@nisi.org>
2013-11-03 07:42:02 -0600

    update sprite and example

--- demos/brown/sprites/SuperBrown.js
+++ demos/brown/sprites/SuperBrown.js
@@ -2,13 +2,13 @@ define([
 	'dojo/_base/declare',
 	'meek/Sprite',
 	'meek/input',
-	'meek/plugin/image!../../superbrown.png',
+	'meek/plugin/image!../../superbrown-all.png',
 	'dojo/keys'
 ], function (declare, Sprite, input, image, keys) {
 	return declare(Sprite, {
 		name: 'brown',
 		image: image,
-		rows: 3,
+		rows: 6,
 		cols: 5,
 		x: 100,
 		y: 100,
@@ -19,8 +19,10 @@ define([
 
 		constructor: function () {
 			this.inherited(arguments);
-			this.addAnimation('waiting',  [7, 8, 7, 8, 7, 8, 6, 6, 6, 6, 6, 5, 5, 5, 5, 6, 6, 6]);
-			this.addAnimation('running',  [0, 1, 2], 0.1);
+			this.addAnimation('waiting-right',  [7, 8, 7, 8, 7, 8, 6, 6, 6, 6, 6, 5, 5, 5, 5, 6, 6, 6]);
+			this.addAnimation('running-right',  [0, 1, 2], 0.1);
+			this.addAnimation('waiting-left',  [17, 8, 17, 8, 17, 18, 16, 16, 16, 16, 16, 15, 15, 15, 15, 16, 16, 16]);
+			this.addAnimation('running-left',  [10, 11, 12], 0.1);
 			input.bind(keys.RIGHT_ARROW, 'right');
 			input.bind(keys.LEFT_ARROW, 'left');
 		},
@@ -29,16 +31,16 @@ define([
 			switch (inputState) {
 			case 'left':
 				this.direction = -1;
-				this.set('animation', 'running');
+				this.set('animation', 'running-left');
 				this.dx = 5;
 				break;
 			case 'right':
 				this.direction = 1;
-				this.set('animation', 'running');
+				this.set('animation', 'running-right');
 				this.dx = 5;
 				break;
 			default:
-				this.set('animation', 'waiting');
+				this.set('animation', 'waiting-right');
 				this.dx = 0;
 				break;
 			}
